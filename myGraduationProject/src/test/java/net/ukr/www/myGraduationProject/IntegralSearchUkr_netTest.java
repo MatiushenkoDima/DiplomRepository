4a8f4dd684266c52d2823ec8abdd7433c375c3f2
MatiushenkoDima <matiushenkoqa@gmail.com>
2017-04-08 13:59:49 +0300

    IntegralSearchUkr.net

---
+++ myGraduationProject/src/test/java/net/ukr/www/myGraduationProject/IntegralSearchUkr_netTest.java
@@ -0,0 +1,56 @@
+package net.ukr.www.myGraduationProject;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class IntegralSearchUkr_netTest {
+@Test
+	public  void testIntegralSearch() {
+		
+		System.setProperty("webdriver.gecko.driver","D:\\SeleniumHQ\\gecodriver\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		//driver.manage().window().maximize();
+	    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("https://www.ukr.net/");
+		
+		
+		////////////////////////////// Search Field
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		
+		String cssOfInputField = "input[name='q']";
+	    WebElement inputFieldQ = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(cssOfInputField)));
+	    try{
+	    	Thread.sleep(4000);
+	    	}
+	    	catch(InterruptedException ie){
+	    	}
+	    inputFieldQ.sendKeys("ukr.net");
+		
+		/////////////////////////////// Button Search
+	    
+	    //driver.findElement(By.xpath("//*[@id='search-form']/div/input[2]")).click();
+	    
+	    String xPathOfSearchButton = "//*[@id='search-form']/div/input[2]";
+	    
+	    WebElement searchButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(xPathOfSearchButton)));
+	    searchButton.click();
+	    try{
+	    	Thread.sleep(3000);
+	    	}
+	    	catch(InterruptedException ie){
+	    	}
+		
+
+		
+		driver.close();
+		driver.quit();
+	}
+
+}
