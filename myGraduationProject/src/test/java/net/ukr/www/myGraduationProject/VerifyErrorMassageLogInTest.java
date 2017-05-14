a7074167f27fcca1dc09536f27e56a11df592200
MatiushenkoDima <matiushenkoqa@gmail.com>
2017-05-14 16:13:38 +0300

    V.Final

--- myGraduationProject/src/test/java/net/ukr/www/myGraduationProject/VerifyErrorMassageLogInTest.java
+++ myGraduationProject/src/test/java/net/ukr/www/myGraduationProject/VerifyErrorMassageLogInTest.java
@@ -14,11 +14,15 @@ public class VerifyErrorMassageLogInTest {
 	public void ErrorMessageLogInTest() {
 		System.setProperty("webdriver.gecko.driver","D:\\SeleniumHQ\\gecodriver\\geckodriver.exe");
 		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		
 		driver.get("https://www.ukr.net/");
 		//Log in Button
 		driver.findElement(By.xpath("//*[@id='user-login-form']/div[2]/div/button")).click();
+		
+		System.out.println("Log in click Button");
+		
 		try {
 			Thread.sleep(2000);
 		} catch (InterruptedException e) {
@@ -29,6 +33,7 @@ public class VerifyErrorMassageLogInTest {
 		//String expected_error="Не вірний логін або пароль. Спробуйте знову."; Старая версия 
 		//String expected_error="Неправильно вказано логін чи пароль. Спробуйте знову."; Новая версия 
 		Assert.assertTrue(actual_error.contains("Неправильно вказано логін чи пароль. Спробуйте знову."));
+		
 		System.out.println("Error message verifyed!");
 		
 		try {
@@ -43,6 +48,7 @@ public class VerifyErrorMassageLogInTest {
 		
 		String link = driver.findElement(By.xpath("//*[@id='login-block']/div/div[2]/div[2]/a")).getText();
 		Assert.assertTrue(link.contains("Чому я не можу увійти у свою поштову скриньку?"));
+		
 		System.out.println("Link verifyed!");
 		
 		try {
@@ -57,7 +63,9 @@ public class VerifyErrorMassageLogInTest {
 		
 		driver.findElement(By.xpath("//*[@id='login-block']/div/div[2]/div[2]/a")).click();
 		driver.findElement(By.xpath("//*[@id='firstHeading']/span")).getText().equals("Error auth");
-		System.out.println("Link work fine!");
+		
+		System.out.println("Error Link work fine!");
+		
 		try {
 			Thread.sleep(2000);
 		} catch (InterruptedException e) {
